dc75b8c3808263f32a72c82c5eaf4d9e6289e753
Salma <salma.sedrati@atos.net>
2026-03-31 11:37:49 +0100

    first commit

---
+++ src/main/java/com/atos/arp/ArpApplication.java
@@ -0,0 +1,13 @@
+package com.atos.arp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atos/arp/ArpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atos.arp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
